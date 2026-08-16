0a8642f08196cda347e387405d16470e1a295e83
Michael <zkdqxaz@gmail.com>
2026-08-16 20:10:25 +0000

    Define subnets

--- labs/01-vpc-foundation/terraform/main.tf
+++ labs/01-vpc-foundation/terraform/main.tf
@@ -20,3 +20,40 @@ resource "aws_vpc" "lab" {
     Name = "aws-cloud-network"
   }
 }
+
+resource "aws_subnet" "public_az1" {
+  vpc_id            = aws_vpc.lab.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-az1"
+  }
+}
+resource "aws_subnet" "public_az2" {
+  vpc_id            = aws_vpc.lab.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "public-az2"
+  }
+}
+resource "aws_subnet" "private_az1" {
+  vpc_id            = aws_vpc.lab.id
+  cidr_block        = "10.10.11.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-az1"
+  }
+}
+resource "aws_subnet" "private_az2" {
+  vpc_id            = aws_vpc.lab.id
+  cidr_block        = "10.10.12.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-az2"
+  }
+}
