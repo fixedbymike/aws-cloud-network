1857293e8244e07dbbcf68714a2232367082282e
Michael <zkdqxaz@gmail.com>
2026-08-16 20:20:55 +0000

    Configure VPC routing

--- labs/01-vpc-foundation/terraform/main.tf
+++ labs/01-vpc-foundation/terraform/main.tf
@@ -57,3 +57,48 @@ resource "aws_subnet" "private_az2" {
     Name = "private-az2"
   }
 }
+resource "aws_internet_gateway" "lab" {
+  vpc_id = aws_vpc.lab.id
+
+  tags = {
+    Name = "aws-cloud-network-igw"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.lab.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.lab.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.lab.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_az1" {
+  subnet_id      = aws_subnet.public_az1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_az2" {
+  subnet_id      = aws_subnet.public_az2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private_az1" {
+  subnet_id      = aws_subnet.private_az1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_az2" {
+  subnet_id      = aws_subnet.private_az2.id
+  route_table_id = aws_route_table.private.id
+}
